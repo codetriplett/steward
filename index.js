a1b0086086df40cb31431ac3d80dd8425651778d
unknown <code.triplett@gmail.com>
2020-03-01 17:38:58 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require('./src/steward')(8080, `${__dirname}/examples`, ({
+	'user-agent': userAgent,
+	cookie: { view }
+}) => ({
+	view: view || /mobile/i.test(userAgent) ? 's' : 'l',
+	category: ({ sku }) => Promise.resolve('appliance') // allows calls to hierarchy service if needed
+}), {
+	'?view=s': 'home-page-s', // { view: 's' }
+	'?view=l': 'home-page-l', // { view: 'l' }
+	'products/{sku}?category=appliance&view=s': 'appliance-s', // { view: 's', name: 'lettuce' }
+	'products/{sku}?category=entertainment&view=l': 'entertainment-l' // { view: 'l', name: 'lettuce' }
+});
---
+++ src/steward.js
@@ -0,0 +1,6 @@
+export default function (...parameters) {
+	// group calls by sets of string, array, object and run through promise all
+	// filter out data from request that has already been cached
+	// cache response data and merge pre cached data in before passing it on
+	// if more than one set was requested, return an array of the results
+}
